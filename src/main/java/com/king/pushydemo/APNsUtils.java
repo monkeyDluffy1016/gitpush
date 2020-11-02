f8f6c5f2b512b026f72ac87c807e3aa16cf31f82
liuyusheng <2046432388@qq.com>
2020-11-02 16:38:29 +0800

    no message

--- src/main/java/com/king/pushydemo/APNsUtils.java
+++ src/main/java/com/king/pushydemo/APNsUtils.java
@@ -11,7 +11,12 @@ import java.util.UUID;
 
 // 修改推送方式   使用VOIP的用这一句   String topic = "com.xxxx.voip";
 //              使用APNS的用这一句   String topic = "com.xxxx";
+//  设置token   String deviceToken ="a4acab463ab55b9342d10abcf47787a6c6cb99881c2268dee312f001c86a2739";
 // 修改     apnsClient = new ApnsClientBuilder().setApnsServer(ApnsClientBuilder.PRODUCTION_APNS_HOST) 这里的 HOST
+// 修改配置的证书，使用p12证书
+// apnsClient = new ApnsClientBuilder().setApnsServer(ApnsClientBuilder.PRODUCTION_APNS_HOST)
+//                        .setClientCredentials(new File("/Users/as/Desktop/apnstuisong.p12"),"123456")
+//                        .setConcurrentConnections(4).setEventLoopGroup(eventLoopGroup).build();
 
 @Slf4j
 public class APNsUtils {
