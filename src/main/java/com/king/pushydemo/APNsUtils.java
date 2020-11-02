00f9d6d9761b0766c68ff41e546d157e7ab5f0fb
liuyusheng <2046432388@qq.com>
2020-11-02 14:45:19 +0800

    添加备注

--- src/main/java/com/king/pushydemo/APNsUtils.java
+++ src/main/java/com/king/pushydemo/APNsUtils.java
@@ -9,7 +9,9 @@ import java.io.File;
 import java.util.Date;
 import java.util.UUID;
 
-// 修改推送方式   VOIP
+// 修改推送方式   使用VOIP的用这一句   String topic = "com.xxxx.voip";
+//              使用APNS的用这一句   String topic = "com.xxxx";
+// 修改     apnsClient = new ApnsClientBuilder().setApnsServer(ApnsClientBuilder.PRODUCTION_APNS_HOST) 这里的 HOST
 
 @Slf4j
 public class APNsUtils {
@@ -17,7 +19,6 @@ public class APNsUtils {
     public static void main(String[] args) throws Exception {
         //IOS等终端设备注册后返回的DeviceToken
         String deviceToken ="a4acab463ab55b9342d10abcf47787a6c6cb99881c2268dee312f001c86a2739";
-//     String deviceToken = "74abd7d51c58a8db995fa53c3508a72481a9d4ad56e7ed1f7d12362f798a6906";
         /**
          * Use the voip push type for notifications that provide information about an incoming Voice-over-IP (VoIP)
          * call. For more information, see Responding to VoIP Notifications from PushKit.
@@ -37,8 +38,6 @@ public class APNsUtils {
         Date invalidationTime= new Date(System.currentTimeMillis() + 60 * 60 * 1000L );
         //发送策略 apns-priority 10为立即 5为省电
         DeliveryPriority priority= DeliveryPriority.IMMEDIATE;
-        //推送方式，主要有alert，background，voip，complication，fileprovider，mdm
-//        PushType pushType = VOIP;
         //推送的合并ID，相同的 apns-collapse-id会在App中合并
         String collapseId= UUID.randomUUID().toString();
         //apnsId 唯一标示，如果不传，APNs会给我们生成一个
@@ -63,23 +62,12 @@ public class APNsUtils {
 
 
                 EventLoopGroup eventLoopGroup = new NioEventLoopGroup(4);
-//                注销掉  * api.development.push.apple.com这个域名苹果官网现在找不到了
-//                apnsClient = new ApnsClientBuilder().setApnsServer(ApnsClientBuilder.DEVELOPMENT_APNS_HOST)
-//这边是正常apns推送的代码
-//                apnsClient = new ApnsClientBuilder().setApnsServer(ApnsClientBuilder.PRODUCTION_APNS_HOST)
-//                        .setClientCredentials(new File("/Users/as/Desktop/apnstuisong.p12"),"123456")
-//                        .setConcurrentConnections(4).setEventLoopGroup(eventLoopGroup).build();
-//这边是VOIP推送
 
                 apnsClient = new ApnsClientBuilder().setApnsServer(ApnsClientBuilder.PRODUCTION_APNS_HOST)
                         .setClientCredentials(new File("/Users/as/Desktop/apnstuisong.p12"),"123456")
                         .setConcurrentConnections(4).setEventLoopGroup(eventLoopGroup).build();
 
-//                EventLoopGroup eventLoopGroup = new NioEventLoopGroup(4);
-//                apnsClient = new ApnsClientBuilder().setApnsServer(ApnsClientBuilder.DEVELOPMENT_APNS_HOST)
-//                        .setSigningKey(ApnsSigningKey.loadFromPkcs8File(new File("/Users/liguoxin/Desktop/p12/deve_push.p8"),
-//                                "我的temid", "我的keyid"))
-//                        .setConcurrentConnections(4).setEventLoopGroup(eventLoopGroup).build();
+
             } catch (Exception e) {
 //                log.error("ios get pushy apns client failed!");
                 System.out.println("ios get pushy apns client failed!");
